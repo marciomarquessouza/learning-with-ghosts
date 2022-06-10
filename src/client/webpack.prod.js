15e5ca38b5eef97c12df99495c0b7201fb804ba1
Marcio Marques de Souza <marciomultimedia@gmail.com>
2022-06-10 21:10:00 +0200

    feat: compress bundle

--- src/client/webpack.prod.js
+++ src/client/webpack.prod.js
@@ -1,8 +1,10 @@
 const { merge } = require('webpack-merge')
+const CompressionPlugin = require('compression-webpack-plugin')
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
     mode: 'production',
+    plugins: [new CompressionPlugin()],
     performance: {
         hints: 'warning',
     },
