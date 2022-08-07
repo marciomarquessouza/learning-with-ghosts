b2fb7a3e547b502864f3599385547cdfa614ec71
Marcio Marques de Souza <marciomultimedia@gmail.com>
2022-08-07 16:05:14 +0200

    feat: add express server

---
+++ src/server/server.ts
@@ -0,0 +1,26 @@
+import express from 'express'
+import path from 'path'
+import http from 'http'
+
+const port = 3000
+
+class App {
+    private server: http.Server
+    private port: number
+
+    constructor(port: number) {
+        this.port = port
+        const app = express()
+        app.use(express.static(path.join(__dirname, '../client')))
+        this.server = new http.Server(app)
+    }
+
+    public start() {
+        this.server.listen(this.port, () => {
+            console.log(`App listening on port: ${this.port}`)
+        })
+    }
+}
+
+const app = new App(port)
+app.start()
